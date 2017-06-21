515d24e01e0c97d1c142310bace3be8ef29d301c
shane_scripted <shane@scripted.org>
2017-06-21 14:40:12 +0000

    new project

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
+$("#red").click(function(){
+ $("body").css("background-color", "red");   
+});
+$("#orange").click(function(){
+ $("body").css("background-color", "orange");   
+});
+$("#yellow").click(function(){
+ $("body").css("background-color", "yellow");   
+});
+$("#green").click(function(){
+ $("body").css("background-color", "green");   
+});
+$("#blue").click(function(){
+ $("body").css("background-color", "blue");   
+});
+$("#purple").click(function(){
+ $("body").css("background-color", "purple");   
+});
 
 });
